73823cc68bf8b786b1dba143e370ce4badfba2c5
Angelos Staboulis <angelosstaboulis@gmail.com>
2024-07-26 06:36:10 +0300

    Commit refactor code

--- ExportHTML/ContentView.swift
+++ ExportHTML/ContentView.swift
@@ -14,38 +14,38 @@ struct ContentView: View {
     @State var isShowings:Bool
     var helper:Helper
     var body: some View {
-        NavigationStack{
-            VStack {
-                Text("Enter your url for Download").frame(width:650,alignment: .leading)
-                TextField("Enter your url for Download",text: $urlDownload).frame(width:650,height:45,alignment: .leading)
-                Text("Enter your path for Saving File").frame(width:650,alignment: .leading)
-                TextField("Enter your path for Saving File",text: $savingFile).frame(width:650,height:45,alignment: .leading)
-                if isShowings {
-                    MediaView(urlFile: urlDownload)
-                        .frame(width: 650, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                }else{
-                    StopMediaView(urlFile: "https://www.google.com")
-                        .frame(width: 650, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Enter your url for Download").frame(width:650,alignment: .leading)
+            TextField("Enter your url for Download",text: $urlDownload).frame(width:650,height:45,alignment: .leading)
+            Text("Enter your path for Saving File").frame(width:650,alignment: .leading)
+            TextField("Enter your path for Saving File",text: $savingFile).frame(width:650,height:45,alignment: .leading)
+            if isShowings {
+                MediaView(urlFile: urlDownload)
+                    .frame(width: 650, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }else{
+                StopMediaView(urlFile: "https://www.google.com")
+                    .frame(width: 650, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            Button {
+                isShowings.toggle()
+            } label: {
+                Text("Show File").frame(width:650,height:45,alignment: .center)
+            }
+            Button(action: {
+                Task{
+                    isDownload.toggle()
+                    helper.downloadfile(urlDownload: urlDownload, save:savingFile)
                 }
-                Button {
-                    isShowings.toggle()
-                } label: {
-                    Text("Show File").frame(width:650,height:45,alignment: .center)
-                }
-                Button(action: {
-                    Task{
-                        isDownload.toggle()
-                        helper.downloadfile(urlDownload: urlDownload, save:savingFile)
-                    }
-                }, label: {
-                    Text("Download file").frame(width:650,height:45,alignment: .center)
-                }).toast(isPresenting: $isDownload) {
-                    AlertToast(type: .complete(.red), title: "Download is fnished!")
-                }
-            }.frame(width:650,height:350)
-        }.onAppear(perform: {
-            savingFile = helper.getDownloadsDirectory()
-        })
+            }, label: {
+                Text("Download file").frame(width:650,height:45,alignment: .center)
+            }).toast(isPresenting: $isDownload) {
+                AlertToast(type: .complete(.red), title: "Download is fnished!")
+            }
+        }.frame(width:650,height:350)
+            .onAppear(perform: {
+                savingFile = helper.getDownloadsDirectory()
+            })
+        
     }
 }
 
